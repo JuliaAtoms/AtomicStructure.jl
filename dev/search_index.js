008ceeeebc5132e7e01286e6f9a20c829791c2cc
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-17 07:43:43 +0000

    build based on 838718e

---
+++ dev/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Atoms.jl-1",
+    "page": "Home",
+    "title": "Atoms.jl",
+    "category": "section",
+    "text": "Documentation for Atoms.jl"
+},
+
+{
+    "location": "density_matrices/#",
+    "page": "Density matrices",
+    "title": "Density matrices",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "density_matrices/#Density-matrices-1",
+    "page": "Density matrices",
+    "title": "Density matrices",
+    "category": "section",
+    "text": "The density matrix operator is defined asbeginequation\nhatrho_ij defd ketbraij\nendequationIf the wavefunction is approximated using Slater determinants, where one Slater determinant of N electrons is defined asbeginequation\nPhi(123N) =\nfrac1sqrtN\nleftbeginmatrix\nchi_1(1)\nchi_2(2)\n\nchi_N(N)\nendmatrixright\nendequationwhere 123N denote the N different electronic coordinates (spatial and spin) and chi_i are the N different spin-orbitals, the transition density matrix for all N electrons, between two Slater determinants Phi_A and Phi_B is given bybeginequation\nbeginaligned\nrho_N^AB(1N1N)\n=\nNPhi_A(1N)conjPhi_B(1N)\n=\nleftbeginmatrix\nrho_1(11)rho_1(1N)\nvdotsddotsvdots\nrho_1(N1)rho_1(NN)\nendmatrixright\nendaligned\nendequation"
+},
+
+{
+    "location": "density_matrices/#References-1",
+    "page": "Density matrices",
+    "title": "References",
+    "category": "section",
+    "text": "Per-Olov Löwdin (1955). Quantum Theory of Many-Particle Systems. I. Physical Interpretations by Means of Density Matrices, Natural Spin-Orbitals, and Convergence Problems in the Method of Configurational Interaction. Physical Review, 97(6), 1474–1489. 10.1103/physrev.97.1474\nPer-Olov Löwdin (1955). Quantum Theory of Many-Particle Systems. II. Study of the Ordinary Hartree-Fock Approximation. Physical Review, 97(6), 1490–1508. 10.1103/physrev.97.1490\nPer-Olov Löwdin (1955). Quantum Theory of Many-Particle Systems. III. Extension of the Hartree-Fock Scheme to Include Degenerate Systems and Correlation Effects. Physical Review, 97(6), 1509–1520. 10.1103/physrev.97.1509"
+},
+
+]}
