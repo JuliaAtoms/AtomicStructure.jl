5f94e10999025cc8dec18e52bfd87404e6a8aad9
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-04-16 11:28:07 +0200

    Extended hydrogenic initialization to find only lowest orbital

--- src/hydrogenic.jl
+++ src/hydrogenic.jl
@@ -1,18 +1,18 @@
 import AtomicLevels: spectroscopic_label
 
 """
-    hydrogenic!(atom[; kwargs...])
+    hydrogenic!(atom[; find_lowest=false, find_lowest_ℓmax=Inf, kwargs...])
 
 Initialize the radial orbitals of `atom` to their unscreened
 hydrogenic values. This is done via simple diagonalization of the
-one-body Hamiltonian for each angular symmetry. The `kwargs` are
-passed on to [`diagonalize_one_body`](@ref) and can be used to
-influence how the diagonalization is performed.
+one-body Hamiltonian for each angular symmetry. If `find_lowest` is
+`true`, only the orbital(s) with the lowest energy is kept (out of
+those with `ℓ≤find_lowest_ℓmax`). The `kwargs` are passed on to
+[`diagonalize_one_body`](@ref) and can be used to influence how the
+diagonalization is performed.
 """
-function hydrogenic!(atom::Atom{T,B,O,TC,C,P}; verbosity=0, kwargs...) where {T,B,O,TC,C,P}
+function hydrogenic!(atom::Atom{T,B,O,TC,C,P}; verbosity=0, find_lowest::Bool=false, find_lowest_ℓmax=Inf, kwargs...) where {T,B,O,TC,C,P}
     # This is only for non-relativistic atoms
-    all_bound(atom) ||
-        throw(ArgumentError("Cannot initialize $(atom) to hydrogenic state if not all orbitals are bound"))
 
     print_block() do io
         verbosity > 0 && println(io, "Hydrogenic initialization of the orbitals of $(atom)")
@@ -43,20 +43,27 @@ function hydrogenic!(atom::Atom{T,B,O,TC,C,P}; verbosity=0, kwargs...) where {T,
         Δeng_fmt = FormatSpec("+10.3e")
         fmt_eng_vec(fspec, v) = "["*join(fmt.(Ref(fspec), v), ", ")*"]"
 
+        λmin = Inf
+
         for ℓ in keys(orbitals)
+            ℓ > find_lowest_ℓmax && continue
             print_block(io) do io
-                min_n = minimum(getn, orbitals[ℓ])
-                max_n = maximum(getn, orbitals[ℓ])
+                min_n,max_n,nev = if any(!isbound, orbitals[ℓ])
+                    ℓ+1,0,1
+                else
+                    min_n = minimum(getn, orbitals[ℓ])
+                    max_n = maximum(getn, orbitals[ℓ])
+
+                    nev = max_n - (min_n-1)
+                    nev ≥ 1 || throw(BoundsError("Trying to find $(nev) eigenvalues"))
+                    min_n,max_n,nev
+                end
 
                 # For shift-and-invert type of diagonalization, we target an
                 # eigenvalue slightly below the lowest energy in partial wave
                 # ℓ of a hydrogen-like system with point charge Z.
                 Iₚℓ = hydrogen_like_eng(min_n)
                 σ = 2Iₚℓ
-
-                max_n = maximum(getn, orbitals[ℓ])
-                nev = max_n - (min_n-1)
-                nev ≥ 1 || throw(BoundsError("Trying to find $(nev) eigenvalues"))
 
                 verbosity > 1 &&
                     println(io, "Diagonalizing symmetry ℓ = $(spectroscopic_label(ℓ)), maximum n = $(max_n) => $(nev) eigenvalues required")
@@ -65,12 +72,23 @@ function hydrogenic!(atom::Atom{T,B,O,TC,C,P}; verbosity=0, kwargs...) where {T,
 
                 H = one_body_hamiltonian(atom, first(orbitals[ℓ]))
                 λᴴ,Φᴴ = diagonalize_one_body(H, nev; σ=σ, io=io, verbosity=verbosity, kwargs...)
-                for orb in orbitals[ℓ]
-                    j = findfirst(isequal(orb), atom.orbitals)
-                    copyto!(view(Φ, :, j), view(Φᴴ, :, getn(orb)-(min_n-1)))
+                if find_lowest
+                    if first(λᴴ) < λmin
+                        λmin = first(λᴴ)
+                        Φ .= zero(T)
+                        for orb in orbitals[ℓ]
+                            j = findfirst(isequal(orb), atom.orbitals)
+                            copyto!(view(Φ, :, j), view(Φᴴ, :, 1))
+                        end
+                    end
+                else
+                    for orb in orbitals[ℓ]
+                        j = findfirst(isequal(orb), atom.orbitals)
+                        copyto!(view(Φ, :, j), view(Φᴴ, :, nev > 1 ? getn(orb)-(min_n-1) : 1))
+                    end
                 end
 
-                if verbosity > 2
+                if verbosity > 2 && max_n > 0
                     λᴴₐ = hydrogen_like_eng.(min_n:max_n)
                     println(io, "Hydrogenic energies $(fmt_eng_vec(eng_fmt, λᴴ)) Ha")
                     println(io, "Analytic energies   $(fmt_eng_vec(eng_fmt, λᴴₐ)) Ha")
@@ -85,7 +103,7 @@ function hydrogenic!(atom::Atom{T,B,O,TC,C,P}; verbosity=0, kwargs...) where {T,
             verbosity > 2 && printfmtln(io, "{1:$(ml)s} {2:9s}  {3:11s}", "", "Initial n", "1-n")
             for j in eachindex(atom.orbitals)
                 n₀ = norm(atom[j])
-                norm_rot!(view(atom, j))
+                iszero(n₀) || norm_rot!(view(atom, j))
 
                 if verbosity > 2
                     n = norm(atom[j])
