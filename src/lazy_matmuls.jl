b74bcc4b3968bb84a472952e51bf43ef710adc1c
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-11-10 16:57:15 +0100

    Provide overload for materialization of MulAdds with adjoint matrices

--- src/lazy_matmuls.jl
+++ src/lazy_matmuls.jl
@@ -7,6 +7,13 @@ for MT in [:Diagonal, :SymTridiagonal, :Tridiagonal]
             ma.C
         end
 
+        function LazyArrays.materialize!(ma::MulAdd{<:Any, <:Any, <:Any, T,
+                                                    <:RadialOperator{<:Any, <:Any, <:Adjoint{<:Any, <:$MT}},
+                                                    Source, Dest}) where {T,Source<:RadialOrbital,Dest<:RadialOrbital}
+            mul!(ma.C.args[2], ma.A.args[2], ma.B.args[2], ma.α, ma.β)
+            ma.C
+        end
+
         ArrayLayouts.default_blasmul!(α, A::$MT, B::AbstractVector, β, C::AbstractVector) =
             mul!(C, A, B, α, β)
     end
