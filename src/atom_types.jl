JuliaAtoms/AtomicStructure.jl

commit 0dd49a36da39d0ee2f3cb93a3eae17275b993dad

Fixed norm(::Atom) for multiconfigurational atoms

src/atom_types.jl
@@ -381,20 +381,22 @@ SCF.orbitals(atom::A) where {A<:Atom} =
     view(atom.radial_orbitals.args[2], :, :)
 
 """
-    norm(atom[, p=2; configuration=1])
+    norm(atom)
 
-This calculates the _amplitude_ norm of the `atom`, i.e. ᵖ√N where N
-is the number electrons. By default, it uses the first `configuration`
-of the `atom` to weight the individual orbital norms.
+This calculates the _amplitude_ norm of the `atom`, i.e. ``√N`` where
+``N`` is the number electrons. Each configuration is weighted by its
+mixing coefficient.
 """
-function LinearAlgebra.norm(atom::Atom{T}; configuration::Int=1) where T
+function LinearAlgebra.norm(atom::Atom{T}) where T
     RT = real(T)
     n = zero(RT)
-    for (orb,occ,state) in outsidecoremodel(get_config(atom.configurations[configuration]),
-                                            atom.potential)
-        j = orbital_index(atom, orb)
-        ϕ = view(atom.radial_orbitals.args[2], :, j)
-        n += occ*dot(ϕ, atom.S, ϕ)
+    for (i,c) in enumerate(atom.mix_coeffs)
+        for (orb,occ,state) in outsidecoremodel(get_config(atom.configurations[i]),
+                                                atom.potential)
+            j = orbital_index(atom, orb)
+            ϕ = view(atom.radial_orbitals.args[2], :, j)
+            n += c^2*occ*dot(ϕ, atom.S, ϕ)
+        end
     end
     Q = num_electrons(outsidecoremodel(first(atom.configurations),
                                        atom.potential))

commit 54a4a9ca772a41ad441b32192ed87c83b7f935c0

Fixed minor bug introduced by 0dd49a3

src/atom_types.jl
@@ -380,6 +380,18 @@ iteration).
 SCF.orbitals(atom::A) where {A<:Atom} =
     view(atom.radial_orbitals.args[2], :, :)
 
+function _norm2(atom::Atom{T}, configuration) where T
+    RT = real(T)
+    n = zero(RT)
+    for (orb,occ,state) in outsidecoremodel(get_config(atom.configurations[configuration]),
+                                            atom.potential)
+        j = orbital_index(atom, orb)
+        ϕ = view(atom.radial_orbitals.args[2], :, j)
+        n += occ*dot(ϕ, atom.S, ϕ)
+    end
+    n
+end
+
 """
     norm(atom)
 
@@ -387,6 +399,23 @@ This calculates the _amplitude_ norm of the `atom`, i.e. ``√N`` where
 ``N`` is the number electrons. Each configuration is weighted by its
 mixing coefficient.
 """
+function LinearAlgebra.norm(atom::Atom{T}) where T
+    n = zero(real(T))
+    for (i,c) in enumerate(atom.mix_coeffs)
+        n += c^2*_norm2(atom, i)
+    end
+    Q = num_electrons(outsidecoremodel(first(atom.configurations),
+                                       atom.potential))
+    √(n/Q)
+end
+
+function LinearAlgebra.norm(atom::Atom{T}, configuration::Integer) where T
+    n = _norm2(atom, configuration)
+    Q = num_electrons(outsidecoremodel(first(atom.configurations),
+                                       atom.potential))
+    √(n/Q)
+end
+
 function LinearAlgebra.norm(atom::Atom{T}) where T
     RT = real(T)
     n = zero(RT)

src/hydrogenic.jl
@@ -125,7 +125,7 @@ function hydrogenic!(atom::Atom{T,B,O,TC,C,P}; verbosity=0, find_lowest::Bool=fa
                 printfmtln(io, "$(configfmt) {2:7s}  {3:11s} ", "Cfg", "Norm", "Norm-1")
                 for (i,config) in enumerate(atom.configurations)
                     i > nconfigs && break
-                    n = norm(atom, configuration=i)
+                    n = norm(atom, i)
                     printfmtln(io, linefmt, config, n, n-1)
                 end
                 length(atom.configurations) > nconfigs && println(io, "⋮")